6e8c14086cb6f814a45b6b5178f27d239ac853a9
Niriksha M <88819709+Nirikshamanoj@users.noreply.github.com>
2023-07-11 12:12:16 +0530

    Update CollatzSequence.java

--- src/CollatzSequence.java
+++ src/CollatzSequence.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CollatzSequence {
+import java.util.Scanner;
+public class CollatzSequence
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number == 1) {
+            System.out.println(1);
+        } else {
+            System.out.println(number);
+            for (int iterator = 0; iterator <= number; iterator++) {
+                if (number % 2 == 0) {
+                    number = number / 2;
+                }
+                else {
+                    number = (3 * number) + 1;
+                }
+                System.out.println(number);
+            }
+        }
+    }
 }
